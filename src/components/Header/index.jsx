4d621bd54520c90dee34a126f145ac2606337365
mcroscia <mcroscia@lifescan.com>
2025-10-08 22:01:15 -0300

    Adicionado suporte para logout no cabeçalho, exibindo nome do usuário logado e ajustando o layout de navegação

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,48 @@
 import React from "react";
 
-import { FaHome, FaSignInAlt, FaUser } from "react-icons/fa";
+import { FaHome, FaSignInAlt, FaUser, FaPowerOff } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
+import * as actions from "../../store/modules/auth/actions";
+import history from "../../services/history";
 import { Nav } from "./styled";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function Header() {
+  const loggedUser = useSelector((state) => state.auth.user?.nome);
+  const dispatch = useDispatch();
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(actions.failureLogin());
+    history.push("/");
+  };
+
   return (
     <Nav>
-      <Link to="/">
-        <FaHome size={24} />
-      </Link>
-      <Link to="/register">
-        <FaSignInAlt size={24} />
-      </Link>
-      <Link to="/login">
-        <FaUser size={24} />
-      </Link>
+      <div></div>
+      <div>
+        <Link to="/">
+          <FaHome size={24} />
+        </Link>
+        <Link to="/register">
+          <FaSignInAlt size={24} />
+        </Link>
+        {loggedUser ? (
+          <Link onClick={handleLogout}>
+            <FaPowerOff size={24} />
+          </Link>
+        ) : (
+          <Link to="/login">
+            <FaUser size={24} />
+          </Link>
+        )}
+      </div>
+      {loggedUser ? (
+        <span>Logado como: {loggedUser}</span>
+      ) : (
+        <span>Faça login para editar registros</span>
+      )}
     </Nav>
   );
 }
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -7,10 +7,23 @@ export const Nav = styled.nav`
   padding: 1rem;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+
+  div {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+  }
+
   a {
     color: ${primaryDarkColor};
     font-weight: bold;
     margin: 0 0.7rem 0;
+  }
+
+  span {
+    flex: 1;
+    text-align: right;
+    padding: 0 1rem;
   }
 `;
